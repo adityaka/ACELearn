8ef2239585d6f3a7a2dc83f55726a65664a24f92
Aditya Kapur <adityaka@live.com>
2019-09-10 14:14:13 +0530

    Stable project build infra and running ACE_Hello binary

--- ACE_Hello/helloace.cpp
+++ ACE_Hello/helloace.cpp
@@ -1,8 +1,10 @@
 #include "ace/OS.h"
 #include "ace/Log_Msg.h"
 int main(int argc, char **argv){
-	ACE_DEBUG((LM_DEBUG, ACE_TEXT("%IMain Entry\n")));
-	ACE_DEBUG(( LM_DEBUG, ACE_TEXT("%IHello world this is an ACE Log\n")));
+
+	ACE_DEBUG((LM_DEBUG,ACE_TEXT("%IMain Entry\n")));
+	ACE_DEBUG((LM_DEBUG,ACE_TEXT("%IHello world this is an ACE Log\n")));
 	ACE_DEBUG((LM_INFO, ACE_TEXT("%IExitting main\n")));
 	return 0;
+
 }
---
+++ CommandLineApp/CommandLineApp.cpp
@@ -0,0 +1,6 @@
+#include<ace/Log_Msg.h>
+#include<ace/OS.h>
+
+int main(int argc, char** argv) {
+
+}
